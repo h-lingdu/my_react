8b34193e64d88ba96b036808a5c07ce8656f1848
h-lingdu <41339042+h-lingdu@users.noreply.github.com>
2020-06-29 19:56:22 +0800

    xiugai

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react'
-import About from './About';
+import React, { Component } from "react";
+import About from "./About";
 
 export default class Home extends Component {
-    constructor() {
-        super();
-        this.state = {
-            msg: '电影'
-        }
-    }
-    render() {
-        return (
-            <div>
-                <About msg={this.state.msg}></About>
-            </div>
-        )
-    }
+  constructor() {
+    super();
+    this.state = {
+      msg: "电影",
+    };
+  }
+  render() {
+    return (
+      <div>
+        <About msg={this.state.msg}></About>
+      </div>
+    );
+  }
 }
